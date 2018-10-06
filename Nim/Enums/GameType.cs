5c3a65bd4a789c73f5fe78e672c0e79db02b513a
ANDRES H CARRERA <andreshcar@live.com>
2018-10-06 15:13:06 -0600

    adding the initial files and folder structure for the main business objects

---
+++ Nim/Enums/GameType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nim.Lib.Enums
+{
+    enum GameType
+    {
+        OnePlayer = 1,
+        TwoPlayer = 2
+    }
+}
